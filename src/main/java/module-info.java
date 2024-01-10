82263776359241ce404baea6501f7771e0d4933e
German Fafenrout <fafenroutg@mail.ru>
2024-01-10 22:29:37 +0300

    init intellij javafx

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module ru.fffafen.fxtest.notes {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens ru.fffafen.fxtest.notes to javafx.fxml;
+    exports ru.fffafen.fxtest.notes;
+}
